77b7e109a867bf1fa4210f1b341d3a635fe5d972
Samuel Chrisdian <samuelchrisdian2003@gmail.com>
2024-07-18 19:57:51 +0700

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from '~/components/_navbar.vue'
+
+const colorMode = useColorMode()
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <NuxtPage class="mx-auto p-4" />
+  </div>
+</template>
+
+<style lang="postcss">
+body {
+  @apply min-h-screen bg-white dark:bg-gray-800 dark:text-gray-200;
+}
+</style>
